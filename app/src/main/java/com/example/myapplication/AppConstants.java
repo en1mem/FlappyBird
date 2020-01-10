f3bb02e634729ab36001e49cbeadfaea7b67f56a
en1mem <germanorumgepp@mail.ru>
2020-01-10 21:06:04 +0500

    check another file

--- app/src/main/java/com/example/myapplication/AppConstants.java
+++ app/src/main/java/com/example/myapplication/AppConstants.java
@@ -8,9 +8,7 @@ import android.view.WindowManager;
 public class AppConstants {
     static BitmapBank bitmapBank;
     static GameEngine gameEngine;
-
-    static int SCREEN_WIDTH;
-    static int SCREEN_HEIGHT;
+    static int SCREEN_WIDTH, SCREEN_HEIGHT;
     static int gravity;
     static int VELOCITY_WHEN_JUMPED;
     static int gapBetweenTopAndBottomTubes;
@@ -22,28 +20,25 @@ public class AppConstants {
     static SoundBank soundBank;
     static Context gameActivityContext;
 
-
-    public static void initialization (Context context) {
+    public static void initialization (Context context){
         setScreenSize(context);
-
         bitmapBank = new BitmapBank(context.getResources());
+        setGameConstants();
         gameEngine = new GameEngine();
         soundBank = new SoundBank(context);
-        setGameConstants();
     }
 
-    public static SoundBank getSoundBank() {
+    public static SoundBank getSoundBank(){
         return soundBank;
     }
 
-
-    public static void setGameConstants() {
+    public static void setGameConstants(){
         AppConstants.gravity = 3;
         AppConstants.VELOCITY_WHEN_JUMPED = -40;
-        AppConstants.gapBetweenTopAndBottomTubes = 600; //experiment
+        gapBetweenTopAndBottomTubes = 600;
         AppConstants.numberOfTubes = 2;
         AppConstants.tubeVelocity = 12;
-        AppConstants.minTubeOffsetY = (int) (AppConstants.gapBetweenTopAndBottomTubes / 2.0);
+        AppConstants.minTubeOffsetY = (int)(AppConstants.gapBetweenTopAndBottomTubes / 2.0);
         AppConstants.maxTubeOffsetY = AppConstants.SCREEN_HEIGHT - AppConstants.minTubeOffsetY - AppConstants.gapBetweenTopAndBottomTubes;
         AppConstants.distanceBetweenTubes = AppConstants.SCREEN_WIDTH * 3 / 4;
     }
@@ -57,14 +52,12 @@ public class AppConstants {
     }
 
     private static void setScreenSize(Context context) {
-        WindowManager windowManager = (WindowManager) context.getSystemService(context.WINDOW_SERVICE);
-        Display display = windowManager.getDefaultDisplay();
-        DisplayMetrics displayMetrics = new DisplayMetrics();
-
-        display.getMetrics(displayMetrics);
-        int width = displayMetrics.widthPixels;
-        int height = displayMetrics.heightPixels;
-
+        WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        Display display = wm.getDefaultDisplay();
+        DisplayMetrics metrics = new DisplayMetrics();
+        display.getMetrics(metrics);
+        int width = metrics.widthPixels;
+        int height = metrics.heightPixels;
         AppConstants.SCREEN_WIDTH = width;
         AppConstants.SCREEN_HEIGHT = height;
     }
